4aa524525e216929f3da96d816bf5b3210178649
ricardo-farias <ricardo.farias@thoughtworks.com>
2022-04-28 11:04:58 -0500

    version controling purchase react app

--- purchase-price-app/src/App.js
+++ purchase-price-app/src/App.js
@@ -7,13 +7,99 @@ function App() {
         <h1>Purchase Price Predictor</h1>
       </header>
       <div>
-        <p>Form Input</p>  
-        <div>
-          <li>Interior</li>
-          <li>Exterior</li>
-          <li>Total</li>
-          <li>Bedrooms</li>
+        <hr></hr>
+        <div className="grid-container">
+          <div id="grid-item">
+            <div id="content">
+              <label>Interior Sqft</label>
+              <input></input>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Exterior Sqft</label>
+              <input></input>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Total Sqft</label>
+              <input></input>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Home Owner Association</label>
+              <select>
+                <option value="true">Has HOA</option>
+                <option value="false">Does not have HOA</option>
+              </select>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Number of Bedrooms</label>
+              <input></input>
+            </div>
+          </div>
+          
+          <div id="grid-item">
+            <div id="content">
+              <label>Number of Baths</label>
+              <input></input>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Driveway</label>
+              <select>
+                <option value="true">Has a driveway</option>
+                <option value="false">Does not have driveway</option>
+              </select>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Garage</label>
+              <select>
+                <option value="true">Has a garage</option>
+                <option value="false">Does not have garage</option>
+              </select>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <label>Year Built</label>
+              <input></input>
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+            </div>
+          </div>
+
+          <div id="grid-item">
+            <div id="content">
+              <button>Submit</button>
+            </div>
+          </div>
         </div>
+
+        <hr></hr>
+        <div className="result-container">
+          <div id="prediction-content">
+            <h2>$1,000,000</h2>
+          </div>
+        </div>
+
       </div>
     </div>
   );
